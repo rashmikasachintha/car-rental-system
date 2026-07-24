408ed9f1640bf231f2d5573982a646c38d99e300
rashmikasachintha <rashmikasachintha02@gmail.com>
2026-07-24 11:55:33 +0530

    Add RegisterRequest DTO with validation constraints"

---
+++ backend/src/main/java/com/carrental/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.carrental.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String phoneNumber;
+}
